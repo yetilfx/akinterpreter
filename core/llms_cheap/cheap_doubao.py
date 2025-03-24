a4bed63d5cfe5653d674000a205e1758dccda085
Fisher Luo <yetilfx@hotmail.com>
2025-03-24 14:19:29 +0800

    添加tushare类库，修订doubao SDK中model参数

--- core/llms_cheap/cheap_doubao.py
+++ core/llms_cheap/cheap_doubao.py
@@ -1,6 +1,12 @@
+from core.utils.config_setting import Config
 from ..llms.doubao_client import DoubaoApiClient
 
 class CheapDoubao(DoubaoApiClient):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.model = "doubao-lite-128k"
+        config = Config()
+        endpoint = config.get("volcengine_doubao")
+        if endpoint :
+            self.model = endpoint
+        else:
+            raise Exception("未配置volcengine_doubao")
